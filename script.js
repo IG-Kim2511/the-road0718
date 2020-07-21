dca8c54e9178cfd7cfda63c3a2770ec6d15b76e6
Ian Kim <59094101+IanKim2511@users.noreply.github.com>
2020-07-21 10:32:12 -0400

    JS 6 navbar icon click - navbar wrapper coming out

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
-// JS 4 .
+// JS 6 navbar icon click - navbar wrapper coming out
+const container = document.querySelector(".container");
+
+// 1-1. const 지정없이 그냥 함수안에 넣어서 코드짜는 방식
+// document.querySelector('.open-navbar-icon').addEventListener(
+//   'click',()={
+//     container.classList.add('change');
+//   }
+// );
+// document.querySelector('.close-navbar-icon').addEventListener(
+//   'click',()={
+//     container.classList.remove('change');
+//   }
+// );
+
+// 1-2. const 지정해서 코드 짜는 방식
+const openNavbar = document.querySelector(".open-navbar-icon");
+const closeNavbar = document.querySelector(".close-navbar-icon");
+
+openNavbar.addEventListener("click", () => {
+  container.classList.add("change");
+});
+closeNavbar.addEventListener("click", () => {
+  container.classList.remove("change");
+});
+
+// JS 4 . Put colors on 'close-navbar-icon'
 // nav-link를 가져와서, array목록의 색을 1개씩  가져와서, background-color 넣음
 // T: array.from , style.cssText
 
